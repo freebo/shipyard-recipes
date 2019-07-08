24166d7280b2c3b968fbf2ab67deb350e4afb29e
Mike Freeman <freeman.mj@gmail.com>
2019-07-08 16:02:13 +1000

    debug basrc

--- OpenFOAM-v1812/docker/run_sample.sh
+++ OpenFOAM-v1812/docker/run_sample.sh
@@ -2,21 +2,32 @@
 
 set -e
 set -o pipefail
+set -x
+echo "Starting!"
+echo "Source intel64"
 
 # set up mpi and set up openfoam env
+
 source $INTELCOMPILERVARS intel64
+echo "source mpivars"
 source /opt/intel/compilers_and_libraries/linux/mpi/bin64/mpivars.sh
 export MPI_ROOT=$I_MPI_ROOT
 OPENFOAM_DIR=/opt/OpenFOAM/OpenFOAM-v1812
+echo "Source bashrc"
+
+cat $OPENFOAM_DIR/etc/bashrc
 source $OPENFOAM_DIR/etc/bashrc
 
 # copy sample into glusterfs shared area
 GFS_DIR=$AZ_BATCH_NODE_SHARED_DIR/gfs
 GFS_DIR=/mnt/resource/batch/tasks/mounts/auto_scratch/openfoamjob
+echo "cd GFS"
 cd $GFS_DIR
+echo "cp"
 cp -r $OPENFOAM_DIR/tutorials/incompressible/simpleFoam/pitzDaily .
 cp $OPENFOAM_DIR/tutorials/incompressible/simpleFoam/pitzDailyExptInlet/system/decomposeParDict pitzDaily/system/
 
+echo "cp done"
 # get nodes and compute number of processors
 IFS=',' read -ra HOSTS <<< "$AZ_BATCH_HOST_LIST"
 nodes=${#HOSTS[@]}
